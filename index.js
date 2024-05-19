e2302f94db60335d8e3e5aa7f4362c0a3a563ea8
Rangho Studio <161280847+Rangho27@users.noreply.github.com>
2024-05-19 22:34:02 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Require the framework and instantiate it
+
+// CommonJs
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ Hello: 'Cloudtype' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
